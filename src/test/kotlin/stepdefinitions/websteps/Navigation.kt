2beec8621cf828273c8efb5e295218cde25550c6
DanielRussellkt <DanielRussell.kt@gmail.com>
2018-05-27 16:26:09 +0100

    Implemented Navigation steps

---
+++ src/test/kotlin/stepdefinitions/websteps/Navigation.kt
@@ -0,0 +1,18 @@
+package stepdefinitions.websteps
+
+import cucumber.api.java8.En
+import drivers.DriverManager.openBrowser
+import drivers.DriverManager.driver
+import drivers.DriverManager.closeBrowser
+
+class Navigation: En {
+    init {
+        Given("^I navigate to \"([^\"&^ ]*)\"") { page: String ->
+            driver().navigate().to(page)
+        }
+
+         Given("^I navigate (back|forward) a page") {direction: String ->
+             if(direction == "back") driver().navigate().back() else driver().navigate().forward()
+         }
+    }
+}
