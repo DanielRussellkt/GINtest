bb4e268ced98e280b758872476e3867b8275d68b
DanielRussellkt <DanielRussell.kt@gmail.com>
2018-05-28 23:54:14 +0100

    Removed thread sleeps now that waiting for elements exists

--- src/test/kotlin/stepdefinitions/websteps/ElementInteraction.kt
+++ src/test/kotlin/stepdefinitions/websteps/ElementInteraction.kt
@@ -11,7 +11,6 @@ import kotlin.test.fail
 class ElementInteraction: En {
     init {
         Given("^I click \"([^\"]*)\"(?: where \"([^\"]*)\" [is|are] \"([^\"]*)\")?") {element: String, properties: String?, values: String? ->
-            Thread.sleep(5_000)
             driver().findElement(By.xpath(
                     ElementLocator.findElementByXpath(generateWebXpath(element, properties?.split(","), values?.split(","))) ?: fail("Element $element not found on page")
             )).click()
--- src/test/kotlin/stepdefinitions/websteps/ElementLocation.kt
+++ src/test/kotlin/stepdefinitions/websteps/ElementLocation.kt
@@ -11,11 +11,10 @@ class ElementLocation: En {
 
     init {
         Given("^I see \"([^\"]*)\"(?: where \"([^\"]*)\" [is|are] \"([^\"]*)\")?") {element: String, properties: String?, values: String? ->
-            Thread.sleep(5_000)
             if (!isElementFound(element, properties, values)) fail("Element $element not found on page")
         }
 
-        Given("^I wait (?: (\\d+) seconds )?until \"([^\"]*)\"(?: where \"([^\"]*)\" [is|are] \"([^\"]*)\")? (appears|disappears)") { timeout: Long?, element: String, properties: String?, values: String?, appears: String ->
+        Given("^I wait (?:(\\d+) seconds )?until \"([^\"]*)\"(?: where \"([^\"]*)\" [is|are] \"([^\"]*)\")? (appears|disappears)") { timeout: Long?, element: String, properties: String?, values: String?, appears: String ->
             when (appears) {
                 "appears" -> if (!waitForElement(element, properties, values, (timeout ?: 20) * 1000)) fail("$element did not appear in ${timeout ?: 20} seconds")
                 "disappears" -> if (!waitForVanish(element, properties, values, (timeout ?: 20) * 1000)) fail("$element did not disappear in ${timeout ?: 20} seconds")
